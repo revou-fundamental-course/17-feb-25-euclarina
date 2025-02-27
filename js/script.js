12d4bebfd6b5e6589ce285ee050255221f89d3fe
Catherine <149693584+euclarina@users.noreply.github.com>
2025-02-27 13:08:19 +0700

    updated autoslide banner

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 // File Eksternal JavaScript
+
+let bannerNum = 0;
+
+function changeBanner(){
+    displayBanner();
+    bannerNum++;
+};
+
+// Function to show and hide the banner
+function displayBanner(){
+    let banner = document.getElementsByClassName('img-banner');
+
+    // Repeat the banner
+    if(bannerNum > banner.length-1){
+        bannerNum = 0;
+    }
+
+    // Hide all banner
+    for (let i = 0; i < banner.length; i++) {
+        banner[i].style.display = 'none';
+    }
+    
+    // Show banner
+    banner[bannerNum].style.display = 'block'; 
+};
+
+setInterval(changeBanner, 3000); // Change banner every 3 seconds
